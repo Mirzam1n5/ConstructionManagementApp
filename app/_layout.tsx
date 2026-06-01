d3f0ba004e24fb55d8d629da682456683fdcf8ce
Mirzam1n5 <milanamelnikova1307@gmail.com>
2026-06-02 01:46:31 +0500

    refactoring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { COLORS } from '../constants';
 import { SheetDataContext, useSheetDataProvider } from '../hooks/useSheetData';
-
-function AppLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: COLORS.white },
-        headerShadowVisible: false,
-        headerTitleStyle: {
-          fontWeight: '500',
-          fontSize: 16,
-          color: COLORS.black,
-        },
-        headerTintColor: COLORS.black,
-        contentStyle: { backgroundColor: COLORS.background },
-      }}
-    />
-  );
-}
 
 export default function RootLayout() {
   const sheetData = useSheetDataProvider();
-
   return (
     <SheetDataContext.Provider value={sheetData}>
-      <StatusBar style="dark" />
-      <AppLayout />
+      <StatusBar style="light" />
+      <Stack screenOptions={{ headerShown: false }} />
     </SheetDataContext.Provider>
   );
 }
