2bf5d4f471b798c40827e7aa66cd78c6ec8b7219
Mirzam1n5 <milanamelnikova1307@gmail.com>
2026-09-07 09:05:56 +0500

    logo fix

--- app/index.tsx
+++ app/index.tsx
@@ -43,7 +43,7 @@ function Logo({size='default'}:{size?:'small'|'default'|'large'}) {
   const s = sizeMap[size];
   return Platform.OS === 'web' ? (
     <img 
-      src="/assets/iskerlogo.png" 
+      src={require('../assets/iskerlogo.png').default || require('../assets/iskerlogo.png')} 
       style={{width: s.w, height: s.h, objectFit: 'contain', display: 'block'}} 
       alt="ISKER Logo" 
     />
